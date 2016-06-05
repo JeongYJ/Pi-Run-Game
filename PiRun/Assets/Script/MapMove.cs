48a54f1ddfa12dc422ec51ee6c6b2b1288bb3714
JeongYJ <wooon00@gmail.com>
2016-06-05 14:35:01 +0900

    2016.06.05_No Design

---
+++ PiRun/Assets/Script/MapMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapMove : MonoBehaviour
+{
+
+    float Speed;
+    float RealSpeed;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Speed = 10f;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Z))
+            RealSpeed = Speed * (float)1.5;
+        else
+            RealSpeed = Speed;
+
+        //transform.Translate(new Vector3(0.0f, 0.0f, -RealSpeed) * Time.deltaTime);
+
+        float posZ = Mathf.Lerp(transform.position.z, transform.position.z - RealSpeed, 1 * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
+
+        if (posZ <= -110)
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 200);
+            
+        }
+}
---
+++ PiRun/Assets/Script/PlayerJump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    float jumpPower = 10f;
+
+    bool isJump = false;
+    // Use this for initialization
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) )
+            isJump = true;
+
+        if (isJump)
+            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+        isJump = false;
+
+    }
+}
